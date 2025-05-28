cfc9cbb4602d9485133978197a1aa7eed3d87f8d
kotha107 <fatemakotha17@gmail.com>
2025-05-28 22:59:35 +0600

    ADD image

---
+++ images/classesPractice1.js
@@ -0,0 +1,10 @@
+class user{
+    constructor(name,email){
+        this.name=name;
+        this.email=email;
+    }
+    viewData(){
+        console.log("some data");
+    }
+}
+let u1 = new user("kotha", "fatemakotha17@gmail.com");
